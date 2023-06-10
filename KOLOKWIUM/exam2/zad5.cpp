cea05a9cc80bf70314bee4e647f2266fb3dce0e2
nullfresh <dawid_olko@outlook.com>
2023-06-10 10:25:40 +0200

    Update zad5.cpp

--- KOLOKWIUM/exam2/zad5.cpp
+++ KOLOKWIUM/exam2/zad5.cpp
@@ -18,3 +18,17 @@ int main()
     cout<<"n wynosi"<<n;
     return 0;
 }
+
+
+// START
+// 1. Zadeklaruj zmienne typu całkowitego x, y, z, n i przypisz wartość 0 do n oraz wartość 1 do y
+// 2. Wczytaj wartość z z klawiatury i zapisz ją do zmiennej z
+// 3. Wykonaj pętlę do-while
+// 4.     Wczytaj wartość x z klawiatury i zapisz ją do zmiennej x
+// 5.     Pomnóż zmienną y przez x i zapisz wynik w zmiennej y
+// 6.     Zwiększ wartość zmiennej n o 1
+// 7. Powtarzaj kroki 4-6 dopóki zmienna y jest mniejsza niż zmienna z
+// 8. Wypisz na ekranie wartość zmiennej y
+// 9. Wypisz na ekranie wartość zmiennej n
+// 10. Zakończ program
+// KONIEC
